f783d3ab46286ecf89f66b0b98c18cc256f434ab
Lukasz Janyst <lukasz@jany.st>
2017-12-27 12:12:30 +0100

    Add the initial state

---
+++ src/reducers/index.js
@@ -0,0 +1,38 @@
+
+const initialCalendarState = {
+  sunday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  monday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  tuesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  wednesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  thursday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  friday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  saturday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  }
+};
