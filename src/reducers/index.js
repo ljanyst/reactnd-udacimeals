799b9b9471da62044652b2aec0e7120ec63c7e6e
Lukasz Janyst <lukasz@jany.st>
2017-12-27 13:33:52 +0100

    Implement the calendar reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,9 @@
 
+import {
+  ADD_RECIPE,
+  REMOVE_FROM_CALENDAR
+} from '../actions';
+
 const initialCalendarState = {
   sunday: {
     breakfast: null,
@@ -36,3 +41,29 @@ const initialCalendarState = {
     dinner: null
   }
 };
+
+function calendar(state = initialCalendarState, action) {
+  const { day, recipe, meal } = action;
+  switch(action.type) {
+  case ADD_RECIPE:
+    return {
+      ...state,
+      [day]: {
+        ...state[day],
+        [meal]: recipe.label
+      }
+    };
+  case REMOVE_FROM_CALENDAR:
+    return {
+      ...state,
+      [day]: {
+        ...state[day],
+        [meal]: null
+      }
+    };
+  default:
+    return state;
+  }
+}
+
+export default calendar;
