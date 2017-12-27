4445bf083eddcc0facea97e638bde3f941134ece
Lukasz Janyst <lukasz@jany.st>
2017-12-27 15:19:32 +0100

    Connect the App component to the redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
   render() {
@@ -12,4 +13,25 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  const dayOrder = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+                    'saturday', 'sunday'];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal];
+        return meals;
+      }, {})
+    }))
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
